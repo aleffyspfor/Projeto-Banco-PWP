6c1fcfc50222accad4f9fd2bf9844e2c366f1d43
Abner Oliveira <abnersanto2014@gmail.com>
2022-05-08 01:15:26 -0300

    Alteração na ação do botão e correção da array

--- Projeto PW FRONT/php/login.php
+++ Projeto PW FRONT/php/login.php
@@ -5,14 +5,14 @@ session_start();
 $cpf = $_POST["cpf"];
 $senha = $_POST["senha"];
 
-if($cpf == "11111111111" && $senha==123)
+if($cpf == "111.111.111-11" && $senha=="123")
 {
-    $_SESSION['abner'] = ["nome": $cpf, "senha": $senha];
+    $_SESSION['abner'] = ["nome"=> $cpf, "senha"=> $senha];
     header("location:./restrito.php");
 }
-else if($cpf == "22222222222" && $senha==123)
+else if($cpf == "222.222.222-22" && $senha=="123")
 {
-    $_SESSION['aleffy'] = ["nome": $cpf, "senha": $senha];
+    $_SESSION['aleffy'] = ["nome"=> $cpf, "senha"=> $senha];
     header("location:./restrito.php");
 }
 else
